785382ed00a39395d0436fc9ea3a10fc73ee221a
Nischay Sharma <nischay876@gmail.com>
2021-12-31 08:06:00 +0530

    Update botconfig.js

--- botconfig.js
+++ botconfig.js
@@ -29,9 +29,9 @@ module.exports = {
      Lavalink: {
       host: process.env.Lavalink_host || "",
       host: process.env.Lavalink_host || "",
-      port: 443,
+      port: 80,
       pass: process.env.Lavalink_pass || "",
-      secure: true,
+      secure: false,
       retryDelay: 5000, // Delay for reconnect in ms
       retryAmount: 1000000000000, // Retry amout if the lavalink is dead and/or restarting.
     },
